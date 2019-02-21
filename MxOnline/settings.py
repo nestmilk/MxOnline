3bffa25e40104cb422ab382d45207e17dfdf8d10
Zhenhua Gong <nestmilk@live.com>
2019-02-21 08:52:52 +0800

    2019.02.21

--- MxOnline/settings.py
+++ MxOnline/settings.py
@@ -20,6 +20,8 @@ import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+#引入apps和extra_app的路径，这样在命令行中也可以启动
 sys.path.insert(0, os.path.join(BASE_DIR,"apps"))
 sys.path.insert(0, os.path.join(BASE_DIR,"extra_app"))
 
